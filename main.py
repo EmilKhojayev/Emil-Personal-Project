d1c16b61e0a2a6f8a94268cdca5dd7afc62d31b7
EmilKhojayev <79109686+EmilKhojayev@users.noreply.github.com>
2021-02-15 22:51:54 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
+import sys
+
+app = QApplication(sys.argv)
+win = QMainWindow()
+win.setGeometry(0, 0, 1920, 1080)
+win.setWindowTitle('TISA Scoreboard')
+
+
+def loginin():
+    if Username.text() == 'SRAA' and Password.text() == 'TISA2021':
+        print('ok')
+        NewWindow = QMainWindow()
+        NewWindow.show()
+
+
+
+
+font = QtGui.QFont()
+font.setFamily("Sitka Text")
+font.setPointSize(22)
+font.setBold(True)
+font.setWeight(75)
+
+
+Userlabel = QtWidgets.QLabel(win)
+Userlabel.setText("Username:")
+Userlabel.setFont(font)
+Userlabel.resize(230, 50)
+Userlabel.move(710, 375)
+
+Passlabel = QtWidgets.QLabel(win)
+Passlabel.setText('Password:')
+Passlabel.setFont(font)
+Passlabel.resize(225, 50)
+Passlabel.move(720, 440)
+
+Username = QtWidgets.QLineEdit(win)
+Username.setFont(font)
+Username.resize(220, 50)
+Username.move(950, 380)
+
+Password = QtWidgets.QLineEdit(win)
+Password.setFont(font)
+Password.resize(220, 50)
+Password.move(950, 450)
+
+Login = QtWidgets.QPushButton(win)
+Login.setFont(font)
+Login.resize(460, 70)
+Login.setText('Login')
+Login.move(710, 530)
+Login.clicked.connect(loginin)
+
+
+
+win.show()
+sys.exit(app.exec_())
